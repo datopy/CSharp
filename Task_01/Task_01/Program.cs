575f2a36d071d2b8202c014906a4a877eedbc144
Dato Kapanadze <dkapanadze@tbcbank.com.ge>
2023-06-12 14:22:10 +0400

    Assignment-05

---
+++ Task_01/Task_01/Program.cs
@@ -0,0 +1,16 @@
+﻿
+SumArrayNumber(new int[] { 1, 2, 1234, 4, 5 }, 2);
+
+void SumArrayNumber(int[] numbers, int index)
+{
+    int sum = 0;
+    string myNumber = numbers[index].ToString();
+    Console.WriteLine(myNumber);
+
+    for (int i = 0; i < myNumber.Length; i++)
+    {
+        int a = myNumber[i] - '0';
+        sum += a;
+    }
+    Console.WriteLine(sum);
+}
