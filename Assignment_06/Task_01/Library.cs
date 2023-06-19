14573c7abfc974b7265fe0a22fc62945aa7da623
DKapanadze <DKapanadze@L_DAKAPANADZE>
2023-06-19 16:58:52 +0400

    Assignment-06 Task_01

---
+++ Assignment_06/Task_01/Library.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarySystem
+{
+    internal class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+
+        public Book(string title, string author, int year)
+        {
+            Title = title;
+            Author = author;
+            Year = year;
+        }
+
+    }
+    internal class Library
+    {
+        private Book[] _books;
+        private int _counter;
+
+        public Library(int libSize)
+        {
+            _books = new Book[libSize];
+            _counter = 0;
+        }
+
+        public Book this[int i]
+        {
+            get { return _books[i];}
+            set { _books[i] = value;}
+        }
+
+        public int BooksCount
+        { 
+            get { return _counter; } 
+        }
+
+        public void AddBook(Book book)
+        {
+            if (_counter > _books.Length)
+            {
+                Console.WriteLine("No more space in Library");
+            }
+            else
+            {
+                _books[_counter] = book;
+                _counter++;
+            }
+        }
+
+        public void RemoveBook(int index)
+        {
+            for (int i = index; i < _books.Length-1; i++)
+            {
+                _books[i] = _books[i + 1];
+            }
+            _counter--;
+        }
+
+        public int FindBook(string title)
+        {
+            int index = 0;
+
+            for (int i = 0; i < _books.Length; i++)
+            {
+                if (_books[i].Title == title)
+                {
+                    index = i;
+                }
+            }
+            return index;
+        }
+
+    }
+}
--- Assignment_06/Task_01/Program.cs
+++ Assignment_06/Task_01/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LibrarySystem;
+
+Book book1 = new Book("A", "A", 1);
+Book book2 = new Book("B", "B", 2);
+Book book3 = new Book("C", "C", 3);
+
+Library library = new Library(3);
+
+library.AddBook(book1);
+library.AddBook(book2);
+library.AddBook(book3);
+
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine(library[i].Title);
+}
+
+Console.WriteLine(library.BooksCount);
+library.RemoveBook(2);
+Console.WriteLine(library.BooksCount);
+
+for (int i = 0; i < 2; i++)
+{
+    Console.WriteLine(library[i].Title);
+}
+
+Console.WriteLine(library.FindBook("B"));
+
